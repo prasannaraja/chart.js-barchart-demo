2783d506e09dc40db6c93edfbbd7b546c62baea1
Prasanna Raja <prasannaraja@msn.com>
2021-03-16 21:17:36 +0400

    other charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BarChartModule } from './shared/bar-chart/bar-chart.module';
 import { LineChartComponent } from './shared/line-chart/line-chart.component';
+import { DoughnutComponent } from './shared/doughnut/doughnut.component';
+import { PiechartComponent } from './shared/piechart/piechart.component';
+import { PolarareaComponent } from './shared/polararea/polararea.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
     LineChartComponent,
+    DoughnutComponent,
+    PiechartComponent,
+    PolarareaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/polararea/polararea.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-polararea',
+  templateUrl: './polararea.component.html',
+  styleUrls: ['./polararea.component.scss']
+})
+export class PolarareaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
